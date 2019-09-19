90be9112ce1b77175fa61093b9b7a731daa37882
SwrkNRK <47861855+SwrkTheTrebel@users.noreply.github.com>
2019-09-19 21:34:55 +0200

    V0.1

---
+++ CV1/cmake-build-debug/CMakeFiles/CV1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CV1.dir/main.c.obj"
+  "CV1.pdb"
+  "CV1.exe"
+  "CV1.exe.manifest"
+  "CV1.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CV1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CV1/cmake-build-debug/CMakeFiles/CV1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CV1.
+# This may be replaced when dependencies are built.
